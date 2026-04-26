9dd8de024dda0a9e50d422c49dce5888bc40d414
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2026-04-26 17:10:11 +1000

    Testing
    Added a test of dynamic brightness

--- host/test.py
+++ host/test.py
@@ -7,9 +7,17 @@ time.sleep(1)
 ser.reset_input_buffer()
 ser.reset_output_buffer()
 
+val = 0
+dir = 1
 out_buffer = [(i, (0,0,0)) for i in range(27)]
- 
-for i in range(27):
-    out_buffer[i] = (i, (0, 255, 255))
 
-ser.write(b"START;" + b";".join(f"{idx},{r},{g},{b}".encode('utf-8') for idx, (r, g, b) in out_buffer) + b";END\n")
+while True:
+    val += 1 * dir
+    if val >= 255 or val <= 0:
+        dir *= -1
+    
+    for i in range(27):
+        out_buffer[i] = (i, (0, val, val))
+
+    ser.write(b"START;" + b";".join(f"{idx},{r},{g},{b}".encode('utf-8') for idx, (r, g, b) in out_buffer) + b";END\n")
+    
