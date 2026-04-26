f69ae4403e45b52dea194c074aaf2936d0f2d200
David Ma <183250896+MadAvidCoder@users.noreply.github.com>
2026-04-26 22:11:41 +1000

    Add matrix
    Added key matrix support

--- client/code.py
+++ client/code.py
@@ -1,6 +1,12 @@
 import board
 import neopixel
 import usb_cdc
+import usb_hid
+import time
+
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keycode import Keycode
 
 pixel_pin = board.P0_08
 length = 27
@@ -11,6 +17,47 @@ for i in range(length):
 pixels.show()
 
 serial = usb_cdc.console
+
+kbd = Keyboard(usb_hid.devices)
+
+ROWS = 4
+COLS = 6
+
+keymap = [
+    Keycode.A, Keycode.B, Keycode.C, Keycode.D, Keycode.E, Keycode.F,
+    Keycode.G, Keycode.H, Keycode.I, Keycode.J, Keycode.K, Keycode.L,
+    Keycode.M, Keycode.N, Keycode.O, Keycode.P, Keycode.Q, Keycode.R,
+    Keycode.S, Keycode.T, Keycode.U, Keycode.V, Keycode.W, Keycode.X
+]
+
+row_pins = [
+    DigitalInOut(board.P1_15),
+    DigitalInOut(board.P1_13),
+    DigitalInOut(board.P1_11),
+    DigitalInOut(board.P0_10),
+]
+
+col_pins = [
+    DigitalInOut(board.P0_11),
+    DigitalInOut(board.P1_00),
+    DigitalInOut(board.P0_24),
+    DigitalInOut(board.P0_22),
+    DigitalInOut(board.P0_20),
+    DigitalInOut(board.P0_17),
+]
+
+for r in row_pins:
+    r.direction = Direction.OUTPUT
+    r.value = True
+
+for c in col_pins:
+    c.direction = Direction.INPUT
+    c.pull = Pull.UP
+
+pressed = [[False for _ in range(COLS)] for _ in range(ROWS)]
+
+def index(r, c):
+    return r * COLS + c
 
 def parse(line):
     try:
@@ -28,6 +75,28 @@ def parse(line):
 print("Active.")
 
 while True:
+    for r in range(ROWS):
+        for row in row_pins:
+            row.value = True
+
+        row_pins[r].value = False
+
+        time.sleep(0.001)
+
+        for c in range(COLS):
+            is_pressed = not col_pins[c].value
+            idx = index(r, c)
+
+            if is_pressed and not pressed[r][c]:
+                pressed[r][c] = True
+                if idx < len(keymap):
+                    kbd.press(keymap[idx])
+
+            elif not is_pressed and pressed[r][c]:
+                pressed[r][c] = False
+                if idx < len(keymap):
+                    kbd.release(keymap[idx])
+
     if serial.in_waiting:
         line = serial.readline().decode('utf-8').strip()
 
